0179ca9c884e920b958329ce0d55b816281bb28e
Alex Faley <alexfaley@gmail.com>
2017-06-21 02:23:40 +0300

    ex1 was done

---
+++ exercise1/step1.py
@@ -0,0 +1,7 @@
+print "Hellow World"
+print "Hello Again"
+print "I Like typing this"
+print "Thih is fun!"
+print "Yah! Printing"
+print "I'd much raather you 'not'"
+print 'I "said" do not touch this'
