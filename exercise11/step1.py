84059502b3e0218d93e8b3d3ed995611e6786ffb
Alex Faley <alexfaley@gmail.com>
2017-06-28 02:49:04 +0300

    ex11 was done. Study input, and small type cast

---
+++ exercise11/step1.py
@@ -0,0 +1,10 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+tall = raw_input()
+print "How much weigh?",
+weight = raw_input()
+
+print "So, uyou're %r old, %r tall and %r heavy."%(
+	age, tall, weight)
+print int(raw_input())*int(10)
