b1ee6c2f4fe6de05e8367b5c54e2d902513c8a02
Alex Faley <alexfaley@gmail.com>
2017-06-29 02:10:31 +0300

    ex15 was done. Read file

---
+++ exercise15/step1.py
@@ -0,0 +1,12 @@
+from sys import argv 
+
+script, filename=argv
+
+txt=open(filename)
+print "Here's your file%r:"%filename
+print txt.read()
+
+print "Type to file name again:"
+file_name_again=raw_input('>')
+txt_again = open(file_name_again)
+print txt_again.read()
