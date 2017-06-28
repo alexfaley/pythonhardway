d5aca5416f96432decd659dd998be39d8047db21
Alex Faley <alexfaley@gmail.com>
2017-06-28 03:22:23 +0300

    ex12 was done. Study prompting

---
+++ exercise12/step1.py
@@ -0,0 +1,6 @@
+age= raw_input("How old are you?")
+tall =  raw_input("How tall are you?")
+weight = raw_input("How much do yoy weight? ")
+
+print "So, you're %r old, %r tall and %r heavy. " %(
+	age, tall, weight)
